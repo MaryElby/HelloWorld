34441658231056704add9aa15b7597390538b989
Mary <melby1@stu.chi.ac.uk>
2020-04-28 16:42:12 +0100

    initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      System.out.println("Hello World!");
+        System.out.println("I love shortcuts");
+        System.out.println("because my typing is very bad");
+    }
+}
